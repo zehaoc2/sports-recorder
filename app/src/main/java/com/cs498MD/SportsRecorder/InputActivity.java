2878e981c4624062ae2bfce5116eb88287320c97
zehaoc2 <chen.jeremy2@gmail.com>
2019-03-10 20:17:40 -0500

    merge all

--- app/src/main/java/com/cs498MD/SportsRecorder/InputActivity.java
+++ app/src/main/java/com/cs498MD/SportsRecorder/InputActivity.java
@@ -1,13 +1,25 @@
 package com.cs498MD.SportsRecorder;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.terry.view.swipeanimationbutton.SwipeAnimationButton;
 import com.terry.view.swipeanimationbutton.SwipeAnimationListener;
 
 public class InputActivity extends Activity {
+
+    private TextView opponentScoreView;
+    private Button opponentAdd;
+    private int opponentScore;
+
+    public static final String OPPONENT = "opponent";
+    public static final String OPPONENT_SCORE = "opponentScore";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,41 @@ public class InputActivity extends Activity {
             }
         });
 
+        opponentScoreView = (TextView) findViewById(R.id.action);
+        opponentAdd = (Button) findViewById(R.id.opponentAdd);
 
+        opponentAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        loadOpponentScore();
+        updateViews();
+
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(OPPONENT, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        loadOpponentScore();
+
+        editor.putInt(OPPONENT_SCORE, opponentScore + 1);
+
+        editor.apply();
+
+        updateViews();
+    }
+
+    public void loadOpponentScore() {
+        SharedPreferences sharedPreferences = getSharedPreferences(OPPONENT, MODE_PRIVATE);
+        opponentScore = sharedPreferences.getInt(OPPONENT_SCORE, 0);
+    }
+
+    public void updateViews() {
+        opponentScoreView.setText(Integer.toString(opponentScore));
     }
 }
