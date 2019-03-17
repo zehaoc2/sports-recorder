31e38176a1ebc6c5aaaae543d06b254c66283d28
Akshaya <akshaya2@illinois.edu>
2019-03-17 17:13:06 -0500

    table looks a bit better

--- app/src/main/java/com/cs498MD/SportsRecorder/GameStats.java
+++ app/src/main/java/com/cs498MD/SportsRecorder/GameStats.java
@@ -1,9 +1,13 @@
 package com.cs498MD.SportsRecorder;
 
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,28 +22,21 @@ public class GameStats extends AppCompatActivity {
 
         TableLayout table = findViewById(R.id.overallStats);
 
-//        Toast.makeText(this, "You got here", Toast.LENGTH_SHORT);
-
-//        <TextView
-//        android:id="@+id/gameText"
-//        android:layout_width="wrap_content"
-//        android:layout_height="wrap_content"
-//        android:text="TextView"
-//        android:background="@drawable/cell_shape"/>
-
-//        GradientDrawable gd = new GradientDrawable();
-//        gd.setColor(0x000);
-//        gd.setCornerRadius(5);
-//        gd.setStroke(2, 0x000);
-
         TableRow header = new TableRow(GameStats.this);
-        header.setBackgroundResource(R.drawable.cell_shape);
-        String[] h = {"", "Total Score", "Period 1", "Period 2", "Period 3", "Period 4", "Period 4+"};
+
+        String[] h = {"", "Total", "Period 1", "Period 2", "Period 3", "Period 4", "Period 4+"};
         String[] teams = {"My Team", "Opponent"};
 
         for (int i = 0; i < 7; i++) {
             TextView tv = new TextView(GameStats.this);
             tv.setText(h[i]);
+            tv.setPadding(8, 0, 8, 0);
+            tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
+            if (i < 6) {
+                tv.setBackgroundResource(R.drawable.cell_shape);
+            }
+
             header.addView(tv);
         }
         table.addView(header);
@@ -47,9 +44,14 @@ public class GameStats extends AppCompatActivity {
 
         for (int i=0; i < 2; i++) {
             TableRow row = new TableRow(GameStats.this);
-            row.setBackgroundResource(R.drawable.cell_shape);
             for (int j = 0; j < 7; j++) {
                 TextView tv = new TextView(GameStats.this);
+                tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                tv.setPadding(8, 0, 8, 0);
+
+                if (i < 6) {
+                    tv.setBackgroundResource(R.drawable.cell_shape);
+                }
 
                 if (j == 0) {
                     tv.setText(teams[i]);
